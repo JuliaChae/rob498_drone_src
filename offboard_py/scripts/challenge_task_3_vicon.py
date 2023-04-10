128a4b94e0c91d849d1833c62e6e9c61769d76ca
JuliaChae <julia.chae2000@gmail.com>
2023-04-10 13:24:23 -0400

    adding challenge task 3 script for vicon

--- offboard_py/scripts/challenge_task_3_vicon.py
+++ offboard_py/scripts/challenge_task_3_vicon.py
@@ -31,7 +31,7 @@ class ChallengeTask3:
         self.T_odom_drone = np.eye(4) # Transform from the drone to the odom frame
         self.current_pose = np.zeros((0,3)) # Current pose of the drone from /mavros/odometry/out
         self.debug = True
-        self.use_vicon = False
+        self.use_vicon = True
 
     def state_cb(self, msg):
         self.current_state = msg
@@ -83,9 +83,9 @@ class ChallengeTask3:
             if i < 3:
                 y_offset = -0.5 
             elif i == 3:
-                y_offset = -0.1
+                y_offset = 0
             else: 
-                y_offset = 0.05
+                y_offset = 0.2
             pos_h = np.array([pose.position.x - 0.3, pose.position.y + 0.3 + y_offset, pose.position.z, 1])
             pos_transformed = np.matmul(self.T_odom_vicon, pos_h.T) # Transform from vicon frame to (pixhawk)odom frame
             pos = pos_transformed[:-1].T
@@ -106,11 +106,11 @@ class ChallengeTask3:
         #print('Waypoints:\n', self.WAYPOINTS)
         if self.waypoint_cnt == -1:
             self.waypoint_cnt = 0
-            if self.use_vicon:
-                self.update_waypoint(self.WAYPOINTS_ORIG[self.waypoint_cnt,:])
-            else:
-                self.current_waypoint = self.WAYPOINTS[self.waypoint_cnt,:]
-                print("FIRST WAYPOINT SET")
+            # if self.use_vicon:
+            #     self.update_waypoint(self.WAYPOINTS_ORIG[self.waypoint_cnt,:])
+            # else:
+            self.current_waypoint = self.WAYPOINTS[self.waypoint_cnt,:]
+            print("FIRST WAYPOINT SET")
             print(self.WAYPOINTS)
             print(self.current_waypoint)
 
@@ -138,17 +138,17 @@ class ChallengeTask3:
             self.pose_vicon_drone = np.eye(4)
             self.pose_vicon_drone[:3,:3] = R[:3,:3]
             self.pose_vicon_drone[:3, 3] = np.array([t.x, t.y, t.z]).T
-        R = quaternion_matrix(np.array([vicon_msg.transform.rotation.x,
-                                        vicon_msg.transform.rotation.y,
-                                        vicon_msg.transform.rotation.z,
-                                        vicon_msg.transform.rotation.w
-                                        ]))
-        t = vicon_msg.transform.translation
-        T_vicon_drone = np.eye(4)
-        T_vicon_drone[:3,:3] = R[:3,:3]
-        T_vicon_drone[:3, 3] = np.array([t.x, t.y, t.z]).T # translation of drone in vicon frame
-        T_drone_odom = np.matmul(np.linalg.inv(T_vicon_drone), np.linalg.inv(self.T_odom_vicon))
-        self.vicon_trans = np.linalg.inv(T_drone_odom)[:3,3]
+        # R = quaternion_matrix(np.array([vicon_msg.transform.rotation.x,
+        #                                 vicon_msg.transform.rotation.y,
+        #                                 vicon_msg.transform.rotation.z,
+        #                                 vicon_msg.transform.rotation.w
+        #                                 ]))
+        # t = vicon_msg.transform.translation
+        # T_vicon_drone = np.eye(4)
+        # T_vicon_drone[:3,:3] = R[:3,:3]
+        # T_vicon_drone[:3, 3] = np.array([t.x, t.y, t.z]).T # translation of drone in vicon frame
+        # T_drone_odom = np.matmul(np.linalg.inv(T_vicon_drone), np.linalg.inv(self.T_odom_vicon))
+        # self.vicon_trans = np.linalg.inv(T_drone_odom)[:3,3]
 
     
     def callback_odom(self, odom_msg):
@@ -204,10 +204,13 @@ class ChallengeTask3:
 
     def update_waypoint(self, waypoint):
 
-        curr_waypoint_h = np.hstack((waypoint, 1))
-        self.T_odom_vicon = np.matmul(self.T_odom_drone, self.linalg.inv(self.pose_vicon_drone))
+        curr_waypoint_h = np.hstack((waypoint, 1)) # waypoint in the vicon frame 
+        self.T_odom_vicon = np.matmul(self.T_odom_drone, self.linalg.inv(self.pose_vicon_drone)) # 
         curr_waypoint_h = np.matmul(self.T_odom_vicon, curr_waypoint_h)
-        self.current_waypoint = curr_waypoint_h[:-1]
+        print("updated current waypoint: ", curr_waypoint_h[:-1])
+        print("original current waypoint: ", self.current_waypoint)
+        self.current_waypoint = self.WAYPOINTS[self.waypoint_cnt,:]
+        # self.current_waypoint = curr_waypoint_h[:-1]
 
     def perturb_from_waypoint(self):
         print("PERTURBING FROM WAYPOINT!!", )
@@ -328,7 +331,7 @@ class ChallengeTask3:
                         # Increment waypoint counter and waypoint                         
                         self.waypoint_cnt += 1
                         if self.use_vicon:
-                            self.current_waypoint = self.WAYPOINTS_ORIG[self.waypoint_cnt, :]
+                            self.update_waypoint(self.WAYPOINTS_ORIG[self.waypoint_cnt, :])
                         else:
                             self.current_waypoint = self.WAYPOINTS[self.waypoint_cnt, :]
             elif self.STATE == 'LAND' or self.waypoint_cnt > self.num_waypoints-1:
